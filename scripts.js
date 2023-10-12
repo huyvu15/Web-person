22e55d7ef1717e41c08a10a13713430bfb12f0d0
VuHuy <huyv80313@gmail.com>
2023-10-12 20:53:26 +0700

    push all

---
+++ scripts.js
@@ -0,0 +1,4 @@
+<div class="products-row1">
+  <button class="shop-button" onclick="window.location.href='https://example.com/shop'">Shop Now</button>
+  <button class="view-button" onclick="window.location.href='3D-photo/photo.html'">View All</button>
+</div>
