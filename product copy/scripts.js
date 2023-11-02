add9aeef1a695fcb46cd394235467738a7689b6a
VuHuy <huyv80313@gmail.com>
2023-11-03 00:03:08 +0700

    push

---
+++ product copy/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 3
+          }
+      }
+  });
+});
